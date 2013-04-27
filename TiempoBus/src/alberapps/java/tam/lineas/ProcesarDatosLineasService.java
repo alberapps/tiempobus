1e1696565adbf8a091ea51f24221b517f0296da7
Alberto Montiel <alberapps@gmail.com>
2013-04-27 02:30:09 +0200

    Cambios en carga de lineas

---
+++ TiempoBus/src/alberapps/java/tam/lineas/ProcesarDatosLineasService.java
@@ -0,0 +1,152 @@
+/**
+ *  TiempoBus - Informacion sobre tiempos de paso de autobuses en Alicante
+ *  Copyright (C) 2012 Alberto Montiel
+ * 
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package alberapps.java.tam.lineas;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.protocol.HTTP;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import alberapps.java.tam.BusLinea;
+import alberapps.java.tam.noticias.Noticias;
+import alberapps.java.util.Utilidades;
+
+/**
+ * 
+ * Procesa los datos recuperados de las lineas
+ * 
+ */
+public class ProcesarDatosLineasService {
+
+	public static final String URL_SUBUS_LINEAS = "http://www.subus.es/Lineas/MapaMapsAgregadorZoom.asp";
+
+	
+	public static void getLineasInfo() throws Exception {
+
+		//List<Noticias> noticias = new ArrayList<Noticias>();
+
+		
+		Document doc = Jsoup.parse(new URL(URL_SUBUS_LINEAS).openStream(), "ISO-8859-1", URL_SUBUS_LINEAS);
+
+		String title = doc.title();
+
+		Elements tables = doc.select("table"); // a with href
+
+		Element tablaGlobal = tables.get(8);
+
+		
+		DatosLinea datosLinea = new DatosLinea();
+		
+		List<DatosLinea> lineas = new ArrayList<DatosLinea>();
+		
+		String tituloCabecera = "";
+		String lineaDesc = "";
+		String lineaNum = "";
+		
+		//Grupo
+		//Element tablaAlicanteUrbano = tables.get(8).select("table").get(1).select("table").get(0);
+		
+		//Titulo cabecera grupo
+		tituloCabecera = tables.get(8).select("table").get(1).select("table").get(0).select("td.cabeza").text();
+		
+		for(int i = 0; i< tables.get(8).select("table").get(1).select("table").get(0).select("td.enlace").select("a").size();i++){
+		
+			//Descripcion linea
+			lineaDesc = tables.get(8).select("table").get(1).select("table").get(0).select("td.enlace").select("a").get(i).attr("title");
+		
+			//Numero linea
+			lineaNum = tables.get(8).select("table").get(1).select("table").get(0).select("td.enlace").select("a").get(i).text();
+		
+		
+			datosLinea = new DatosLinea();
+			datosLinea.setTituloCabecera(tituloCabecera);
+			datosLinea.setLineaDescripcion(lineaDesc);
+			datosLinea.setLineaNum(lineaNum);
+			
+			lineas.add(datosLinea);
+		}
+		
+		
+		
+		//urbano : tables.get(8).select("table").get(1).select("table").get(0)
+		
+		//titulo urbano:  tables.get(8).select("table").get(1).select("table").get(0).select("td.cabeza") 
+		
+		//elementos: tables.get(8).select("table").get(1).select("table").get(0).select("td.enlace")
+		
+		//links valor: tables.get(8).select("table").get(1).select("table").get(0).select("td.enlace").select("a")
+		
+		//textos lineas: tables.get(8).select("table").get(1).select("table").get(0).select("td.enlace").select("a")
+		
+		//titulo tables.get(8).select("table").get(1).select("table").get(0).select("td.enlace").select("a").get(0).attr("title")
+		
+		//javascript link : tables.get(8).select("table").get(1).select("table").get(0).select("td.enlace").select("input").get(0).attr("onclick")
+		
+		//tables.get(8).select("table").get(1).select("table").get(1) elemento
+		
+		
+		
+		/*Elements filas = tabla.select("tr");
+
+		for (int i = 0; i < filas.size(); i++) {
+
+			if (filas.get(i).select("td").size() == 2) {
+				Noticias noticia = new Noticias();
+
+				noticia.setFecha(filas.get(i).select("td").get(0).text());
+
+				noticia.setNoticia(filas.get(i).select("td").get(1).text());
+
+				noticia.setLinks(new ArrayList<String>());
+				noticia.setDescLink(new ArrayList<String>());
+
+				Elements links = filas.get(i).select("td").get(1).select("a[href]");
+
+				for (Element link : links) {
+
+					noticia.getLinks().add(link.attr("abs:href"));
+					noticia.getDescLink().add(link.text());
+
+				}
+
+				noticias.add(noticia);
+
+				
+				
+			}
+
+		}
+		if(!noticias.isEmpty()){
+			noticias.remove(0);
+		}
+		
+		return noticias;
+*/
+	}
+
+}
