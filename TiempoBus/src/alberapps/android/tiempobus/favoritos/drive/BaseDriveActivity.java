23063cf68b2234368cdda7a06a7c09a5f2218f2f
Alberto Montiel <alberapps@gmail.com>
2014-01-22 21:16:25 +0100

    drive api

---
+++ TiempoBus/src/alberapps/android/tiempobus/favoritos/drive/BaseDriveActivity.java
@@ -0,0 +1,129 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+
+package alberapps.android.tiempobus.favoritos.drive;
+
+import alberapps.android.tiempobus.actionbar.ActionBarActivity;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+
+public class BaseDriveActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+	private static final String TAG = "BaseDriveActivity";
+
+	/**
+	 * Extra for account name.
+	 */
+	protected static final String EXTRA_ACCOUNT_NAME = "account_name";
+
+	/**
+	 * Request code for auto Google Play Services error resolution.
+	 */
+	protected static final int REQUEST_CODE_RESOLUTION = 1;
+
+	/**
+	 * Next available request code.
+	 */
+	protected static final int NEXT_AVAILABLE_REQUEST_CODE = 2;
+
+	/**
+	 * Google API client.
+	 */
+	protected GoogleApiClient mGoogleApiClient;
+
+	/**
+	 * Called when activity gets visible. A connection to Drive services need to
+	 * be initiated as soon as the activity is visible. Registers
+	 * {@code ConnectionCallbacks} and {@code OnConnectionFailedListener} on the
+	 * activities itself.
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (mGoogleApiClient == null) {
+			mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(Drive.API).addScope(Drive.SCOPE_FILE).addConnectionCallbacks(this).addOnConnectionFailedListener(this).build();
+		}
+		mGoogleApiClient.connect();
+	}
+
+	/**
+	 * Handles resolution callbacks.
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == REQUEST_CODE_RESOLUTION && resultCode == RESULT_OK) {
+			mGoogleApiClient.connect();
+		}
+	}
+
+	/**
+	 * Called when activity gets invisible. Connection to Drive service needs to
+	 * be disconnected as soon as an activity is invisible.
+	 */
+	@Override
+	protected void onPause() {
+		if (mGoogleApiClient != null) {
+			mGoogleApiClient.disconnect();
+		}
+		super.onPause();
+	}
+
+	/**
+	 * Called when {@code mGoogleApiClient} is connected.
+	 */
+
+	public void onConnected(Bundle connectionHint) {
+		Log.i(TAG, "GoogleApiClient connected");
+	}
+
+	/**
+	 * Called when {@code mGoogleApiClient} is disconnected.
+	 */
+
+	public void onDisconnected() {
+		Log.i(TAG, "GoogleApiClient disconnected");
+	}
+
+	/**
+	 * Called when {@code mGoogleApiClient} is trying to connect but failed.
+	 * Handle {@code result.getResolution()} if there is a resolution is
+	 * available.
+	 */
+
+	public void onConnectionFailed(ConnectionResult result) {
+		Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
+		if (!result.hasResolution()) {
+			// show the localized error dialog.
+			GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this, 0).show();
+			return;
+		}
+		try {
+			result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
+		} catch (SendIntentException e) {
+			Log.e(TAG, "Exception while starting resolution activity", e);
+		}
+	}
+
+	/**
+	 * Shows a toast message.
+	 */
+	public void showMessage(String message) {
+		Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+	}
+
+	/**
+	 * Getter for the {@code GoogleApiClient}.
+	 */
+	public GoogleApiClient getGoogleApiClient() {
+		return mGoogleApiClient;
+	}
+
+}
---
+++ TiempoBus/src/alberapps/android/tiempobus/favoritos/drive/FavoritoDriveActivity.java
@@ -0,0 +1,97 @@
+/**
+ *  TiempoBus - Informacion sobre tiempos de paso de autobuses en Alicante
+ *  Copyright (C) 2012 Alberto Montiel
+ * 
+ *  based on code by ZgzBus Copyright (C) 2010 Francho Joven
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package alberapps.android.tiempobus.favoritos.drive;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.IntentSender.SendIntentException;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi.ContentsResult;
+import com.google.android.gms.drive.DriveApi.OnNewContentsCallback;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.OpenFileActivityBuilder;
+
+/**
+ * Guarda un nuevo favorito
+ * 
+ * 
+ */
+@SuppressLint("NewApi")
+public class FavoritoDriveActivity extends BaseDriveActivity  {
+
+	private static final String TAG = "CreateFileWithCreatorActivity";
+
+    protected static final int REQUEST_CODE_CREATOR = 1;
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        super.onConnected(connectionHint);
+        OnNewContentsCallback onContentsCallback = new OnNewContentsCallback() {
+
+            
+            public void onNewContents(ContentsResult result) {
+                MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
+                        .setMimeType("text/html").build();
+                IntentSender intentSender = Drive.DriveApi
+                        .newCreateFileActivityBuilder()
+                        .setInitialMetadata(metadataChangeSet)
+                        .setInitialContents(result.getContents())
+                        .build(getGoogleApiClient());
+                try {
+                    startIntentSenderForResult(
+                            intentSender, REQUEST_CODE_CREATOR, null, 0, 0, 0);
+                } catch (SendIntentException e) {
+                  Log.w(TAG, "Unable to send intent", e);
+                }
+            }
+        };
+        Drive.DriveApi.newContents(getGoogleApiClient()).addResultCallback(onContentsCallback);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+        case REQUEST_CODE_CREATOR:
+            if (resultCode == RESULT_OK) {
+                DriveId driveId = (DriveId) data.getParcelableExtra(
+                        OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
+                showMessage("File created with ID: " + driveId);
+                
+               /* if(driveId == null){
+                	
+                	mGoogleApiClient.connect();
+                	
+                }*/
+                
+            }
+            //finish();
+            break;
+        default:
+            super.onActivityResult(requestCode, resultCode, data);
+            break;
+        }
+    }
+
+}
